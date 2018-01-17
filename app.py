6b4c632cfdaa6e3810256a7fc08e3f216cb69754
ollivilmi <olli.vilmi@gmail.com>
2018-01-17 14:11:04 +0200

    Basic template for a web page

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, url_for, g, jsonify
+from flask_jsglue import JSGlue
 
 app = Flask(__name__)
+jsglue = JSGlue(app)
 
 @app.route('/')
 def index():
-	return "Hello"
+	return render_template("index.html")
+	
+@app.route('/experience')
+def experience():
+	return render_template("experience.html")
+	
+@app.route('/hobbies')
+def hobbies():
+	return render_template("hobbies.html")
+
+@app.route('/personality')
+def personality():
+	return render_template("personality.html")
 	
 		
 if __name__ == "__main__":
